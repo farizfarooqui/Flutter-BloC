b001c856b50918fdea774187188b477773a3c1f4
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-02-26 05:54:56 +0500

    multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:blocpract/HomeView/bloc/home_bloc.dart';
-import 'package:blocpract/HomeView/counterscreen.dart';
 import 'package:blocpract/switch_screen/bloc/switch_bloc.dart';
-import 'package:blocpract/switch_screen/bloc/switch_screen.dart';
+import 'package:blocpract/switch_screen/switch_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,13 +13,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    //   home: SwitchScreen(),
-    // );
-
-    return BlocProvider(
-        create: (_) => SwitchBloc(),
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => HomeBloc(),
+          ),
+          BlocProvider(
+            create: (context) => SwitchBloc(),
+          ),
+        ],
         child: const MaterialApp(
-            debugShowCheckedModeBanner: false, home: SwitchScreen()));
+          debugShowCheckedModeBanner: false,
+          home: SwitchScreen(),
+        ));
   }
 }
