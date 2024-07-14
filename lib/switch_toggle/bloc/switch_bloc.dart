e71b1be8e36d36eeb82c52b4d4434f975d02e4b3
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-07-14 23:32:21 +0500

    .

--- lib/switch_toggle/bloc/switch_bloc.dart
+++ lib/switch_toggle/bloc/switch_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 part 'switch_event.dart';
 part 'switch_state.dart';
 
